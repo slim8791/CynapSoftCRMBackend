10a3a8ab88f98d72aa3a5afad67129ca9f794592
Ahmed Benjdidia <Benjdidia.ahmed@gmail.com>
2026-04-01 18:46:03 +0100

    Creation CRUD produits

---
+++ CynapCRM.Services.ProductAPI/Models/Dto/PromotionDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CynapCRM.Services.ProductAPI.Models.Dto
+{
+    public class PromotionDto
+    {
+        public int Id_Promo { get; set; }
+        public string CodePromo { get; set; } = string.Empty;
+        public float Pourcentage { get; set; }
+        public DateTime DateDebut { get; set; }
+        public DateTime DateExpiration { get; set; }
+        public bool EstActive { get; set; }
+        public string NumeroLot { get; set; } = string.Empty;
+    }
+}
---
+++ CynapCRM.Services.ProductAPI/Models/Dto/SupportMarketingDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CynapCRM.Services.ProductAPI.Models.Dto
+{
+    public class SupportMarketingDto
+    {
+        public int Id_SupportMarketting { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public int Id_Produit { get; set; }
+        public IEnumerable<FichierDto>? Fichiers { get; set; }
+    }
+}
