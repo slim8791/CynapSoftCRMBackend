707e8f178a8ba711d32da948620b64f3b026ba27
Ahmed Benjdidia <Benjdidia.ahmed@gmail.com>
2026-04-02 11:58:53 +0100

    Division des méthodes HTTP du micro service Product successfull

---
+++ CynapCRM.Services.ProductAPI/Controllers/PromoController.cs
@@ -0,0 +1,72 @@
+﻿using CynapCRM.Services.ProductAPI.Models.Dto;
+using CynapCRM.Services.ProductAPI.Service.IService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CynapCRM.Services.ProductAPI.Controllers
+{
+    [Route("api/promo")]
+    [ApiController]
+    [Authorize]
+    public class PromoController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        protected ResponseDto _response;
+        public PromoController(IProductService productService)
+
+        {
+
+            _productService = productService;
+            _response = new();
+        }
+
+        [HttpGet("promotions")]
+        public async Task<ResponseDto> GetAllPromotions()
+        {
+            try
+            {
+                _response.Result = await _productService.GetPromotionsAsync();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpPost("promotion")]
+        [Authorize(Roles = "ADMIN")]
+        public async Task<ResponseDto> CreateUpdatePromotion([FromBody] PromotionDto promotionDto)
+        {
+            try
+            {
+                _response.Result = await _productService.CreateUpdatePromotionAsync(promotionDto);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpDelete("promotion/{id:int}")]
+        [Authorize(Roles = "ADMIN")]
+        public async Task<ResponseDto> DeletePromotion(int id)
+        {
+            try
+            {
+                _response.Result = await _productService.DeletePromotionAsync(id);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+    }
+}
